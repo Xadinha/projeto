6068654bf93dcae4212caeb1ec91b006f57c234a
bernardo <bernardo@LAPTOP-5B87J3EC>
2024-01-04 10:42:02 +0000

    Add project files.

---
+++ Projeto loja de roupa com Rafael Cordeiro/Form1.cs
@@ -0,0 +1,10 @@
+namespace Projeto_loja_de_roupa_com_Rafael_Cordeiro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projeto loja de roupa com Rafael Cordeiro/Program.cs
@@ -0,0 +1,17 @@
+namespace Projeto_loja_de_roupa_com_Rafael_Cordeiro
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
