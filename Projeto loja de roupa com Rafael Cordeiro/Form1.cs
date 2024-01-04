04acf7a3b815a9768be3ff5078df257e482659dc
bernardo <bernardo@LAPTOP-5B87J3EC>
2024-01-04 12:29:36 +0000

    Página principal feita ideias e desenvolvimento do trabalho realizado começo do word

--- Projeto loja de roupa com Rafael Cordeiro/Form1.cs
+++ Projeto loja de roupa com Rafael Cordeiro/Form1.cs
@@ -6,5 +6,25 @@ namespace Projeto_loja_de_roupa_com_Rafael_Cordeiro
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
